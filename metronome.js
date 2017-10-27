6821bf767bc1d96831a32909d06027e986c6962c
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2017-10-27 21:28:42 +0100

    first commit

---
+++ metronome.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  let playStop = document.getElementById('play-stop');
+
+  playStop.onclick = () => playStop.style.color = '#FFFFFF';
+
+});
