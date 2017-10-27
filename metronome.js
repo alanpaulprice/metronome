2e4c09d22e52e474420415d708d45388c4a897dd
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2017-10-27 22:07:04 +0100

    added play/stop functionality and timing mechanism

--- metronome.js
+++ metronome.js
@@ -1,9 +1,29 @@
 /*jshint esversion: 6 */
 
 document.addEventListener('DOMContentLoaded', function () {
+  console.clear();
 
+  let bpm = 60;
+  let playing = false;
   let playStop = document.getElementById('play-stop');
 
-  playStop.onclick = () => playStop.style.color = '#FFFFFF';
+  playStop.onclick = () => {
+    if (!playing) {
+      playing = true;
+      metronomeAudio();
+    } else {
+      playing = false;
+    }
+  }
+
+  function metronomeAudio() {
+
+    if (playing) {
+      console.log("Hello"); //TODO: Change to a click
+      setTimeout(() => {
+        metronomeAudio();
+      }, 60000/bpm)
+    }
+  }
 
 });
