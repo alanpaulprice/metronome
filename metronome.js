c2d80b4fefe9a3d032ce412a8bacbed2d6d0cd35
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2017-12-06 18:55:46 +0000

    changed onclicks to addeventlisteners, added m qry for short screens

--- metronome.js
+++ metronome.js
@@ -22,19 +22,19 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // ===== MINUS BUTTON =====
-  minusButton.onclick = () => {
+  minusButton.addEventListener("click", () => {
     if (bpm <= 50) return;
     bpm--;
     updateReadout();
-  }
+  });
   // ===== PLUS BUTTON =====
-  plusButton.onclick = () => {
+  plusButton.addEventListener("click", () => {
     if (bpm >= 190) return;
     bpm++;
     updateReadout();
-  }
+  });
   // ===== PLAY / STOP BUTTON =====
-  playStop.onclick = () => {
+  playStop.addEventListener("click", () => {
     if (!playing) {
       playing = true;
       playStop.classList.toggle("fa-play");
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
       playStop.classList.toggle("fa-play");
       playStop.classList.toggle("fa-stop");
     }
-  }
+  });
   // ===== METRONOME AUDIO =====
   // if nome engaged, reset audio n play it,
   // wait a beat before recursively calling func again
@@ -65,21 +65,21 @@ document.addEventListener('DOMContentLoaded', function() {
     updateReadout();
   }
 
-  document.getElementById("bpm-cell-50").onclick = () => bpmTableClick(50);
-  document.getElementById("bpm-cell-60").onclick = () => bpmTableClick(60);
-  document.getElementById("bpm-cell-70").onclick = () => bpmTableClick(70);
-  document.getElementById("bpm-cell-80").onclick = () => bpmTableClick(80);
-  document.getElementById("bpm-cell-90").onclick = () => bpmTableClick(90);
-  document.getElementById("bpm-cell-100").onclick = () => bpmTableClick(100);
-  document.getElementById("bpm-cell-110").onclick = () => bpmTableClick(110);
-  document.getElementById("bpm-cell-120").onclick = () => bpmTableClick(120);
-  document.getElementById("bpm-cell-130").onclick = () => bpmTableClick(130);
-  document.getElementById("bpm-cell-140").onclick = () => bpmTableClick(140);
-  document.getElementById("bpm-cell-150").onclick = () => bpmTableClick(150);
-  document.getElementById("bpm-cell-160").onclick = () => bpmTableClick(160);
-  document.getElementById("bpm-cell-170").onclick = () => bpmTableClick(170);
-  document.getElementById("bpm-cell-180").onclick = () => bpmTableClick(180);
-  document.getElementById("bpm-cell-190").onclick = () => bpmTableClick(190);
+  document.getElementById("bpm-cell-50").addEventListener("click", () => bpmTableClick(50));
+  document.getElementById("bpm-cell-60").addEventListener("click", () => bpmTableClick(60));
+  document.getElementById("bpm-cell-70").addEventListener("click", () => bpmTableClick(70));
+  document.getElementById("bpm-cell-80").addEventListener("click", () => bpmTableClick(80));
+  document.getElementById("bpm-cell-90").addEventListener("click", () => bpmTableClick(90));
+  document.getElementById("bpm-cell-100").addEventListener("click", () => bpmTableClick(100));
+  document.getElementById("bpm-cell-110").addEventListener("click", () => bpmTableClick(110));
+  document.getElementById("bpm-cell-120").addEventListener("click", () => bpmTableClick(120));
+  document.getElementById("bpm-cell-130").addEventListener("click", () => bpmTableClick(130));
+  document.getElementById("bpm-cell-140").addEventListener("click", () => bpmTableClick(140));
+  document.getElementById("bpm-cell-150").addEventListener("click", () => bpmTableClick(150));
+  document.getElementById("bpm-cell-160").addEventListener("click", () => bpmTableClick(160));
+  document.getElementById("bpm-cell-170").addEventListener("click", () => bpmTableClick(170));
+  document.getElementById("bpm-cell-180").addEventListener("click", () => bpmTableClick(180));
+  document.getElementById("bpm-cell-190").addEventListener("click", () => bpmTableClick(190));
 
   // ===== VOLUME CONTROL =====
   // converts output of slider to useable values and updates volume n display
