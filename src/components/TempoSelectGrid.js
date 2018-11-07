af7187f52ac66aca651abb4604844fb74e5dce59
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-11-07 22:57:34 +0000

    TempoSelectGrid: replaced button onClick events with one on parent div

--- src/components/TempoSelectGrid.js
+++ src/components/TempoSelectGrid.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
 const TempoSelectGrid = ({ setTempo }) => {
-  const onGridButtonClick = value => setTempo(value);
+  const onGridButtonClick = e => setTempo(e.target.value);
+
   // An array containing the values 40 to 230
   const gridButtonValues = [...Array(20)].map(
     (curr, ind) => (ind + 1) * 10 + 30
   );
 
   const GridButtons = gridButtonValues.map(curr => (
-    <button
-      className="gridButton"
-      key={curr}
-      onClick={() => onGridButtonClick(curr)}
-    >
+    <button className="gridButton" key={curr} value={curr}>
       {curr}
     </button>
   ));
 
-  return <div>{GridButtons}</div>;
+  return (
+    <div className="TempoSelectGrid" onClick={onGridButtonClick}>
+      {GridButtons}
+    </div>
+  );
 };
 
 export default TempoSelectGrid;
