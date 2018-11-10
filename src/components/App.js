2aa3b1ee86a8bb3bd7bc9f3e3b2aa2796b7f58f5
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-11-10 22:07:01 +0000

    replaced html buttons with styled.Button in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import TempoSelectGrid from './TempoSelectGrid';
 import KeyboardShortcuts from './KeyboardShortcuts';
+import Button from './Button';
 
 const withinAllowedRange = num => num >= 40 && num <= 230;
 
@@ -60,52 +61,55 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={{ theme }}>
-      <div className="App">
-        <h1>metronome</h1>
+        <div className="App">
+          <h1>metronome</h1>
 
-        <button
-          className="incrementButton"
-          onClick={this.onIncrementButtonMinusClick}
-        >
-          <i className="fa fa-minus" />
-        </button>
+          <Button
+            className="incrementButton"
+            onClick={this.onIncrementButtonMinusClick}
+          >
+            <i className="fa fa-minus" />
+          </Button>
 
-        <form onSubmit={this.onInputFormSubmit}>
-          <input
-            className="tempoInput"
-            ref={this.TempoInputRef}
-            value={this.state.tempoInputValue}
-            onChange={this.onTempoInputChange}
-            onFocus={this.onInputFocus}
-            onBlur={this.onInputBlur}
-          />
-        </form>
+          <form onSubmit={this.onInputFormSubmit}>
+            <input
+              className="tempoInput"
+              ref={this.TempoInputRef}
+              value={this.state.tempoInputValue}
+              onChange={this.onTempoInputChange}
+              onFocus={this.onInputFocus}
+              onBlur={this.onInputBlur}
+            />
+          </form>
 
-        <button
-          className="incrementButton"
-          onClick={this.onIncrementButtonPlusClick}
-        >
-          <i className="fa fa-plus" />
-        </button>
+          <Button
+            className="incrementButton"
+            onClick={this.onIncrementButtonPlusClick}
+          >
+            <i className="fa fa-plus" />
+          </Button>
 
-        <button className="playStopButton" onClick={this.onPlayStopButtonClick}>
-          <i className={`fa fa-${this.state.playing ? 'stop' : 'play'}`} />
-        </button>
+          <Button
+            className="playStopButton"
+            onClick={this.onPlayStopButtonClick}
+          >
+            <i className={`fa fa-${this.state.playing ? 'stop' : 'play'}`} />
+          </Button>
 
-        <TempoSelectGrid setTempo={this.setTempo} />
+          <TempoSelectGrid setTempo={this.setTempo} />
 
-        <div className="volumeControl">
-          <input
-            type="range"
-            step="10"
-            value={this.state.volume}
-            onChange={this.onVolumeInputChange}
-          />
-          <div className="volumeReadout">volume: {this.state.volume}%</div>
+          <div className="volumeControl">
+            <input
+              type="range"
+              step="10"
+              value={this.state.volume}
+              onChange={this.onVolumeInputChange}
+            />
+            <div className="volumeReadout">volume: {this.state.volume}%</div>
+          </div>
+
+          <KeyboardShortcuts />
         </div>
-
-        <KeyboardShortcuts />
-      </div>
       </ThemeProvider>
     );
   }
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default ({ children }) => {
+  const Button = styled.button``;
+
+  return <Button>{children}</Button>;
+};
