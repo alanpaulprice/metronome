0e574f02c578dc63b5a48acfd71ae975a47c8495
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-11-11 16:45:08 +0000

    deleted index.css, added GlobalStyle in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import React, { Component, Fragment } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import TempoSelectGrid from './TempoSelectGrid';
 import KeyboardShortcuts from './KeyboardShortcuts';
 import Button from '../elements/Button';
@@ -10,6 +10,15 @@ const theme = {
   main: 'hsl(34, 78%, 91%)',
   secondary: 'hsl(34, 76%, 15%)'
 };
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: monospace;
+  }
+`;
 
 class App extends Component {
   state = { tempo: 120, tempoInputValue: '120', playing: false, volume: 100 };
@@ -61,55 +70,58 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={{ theme }}>
-        <div className="App">
-          <h1>metronome</h1>
+        <Fragment>
+          <GlobalStyle />
+          <div className="App">
+            <h1>metronome</h1>
 
-          <Button
-            className="incrementButton"
-            onClick={this.onIncrementButtonMinusClick}
-          >
-            <i className="fa fa-minus" />
-          </Button>
+            <Button
+              className="incrementButton"
+              onClick={this.onIncrementButtonMinusClick}
+            >
+              <i className="fa fa-minus" />
+            </Button>
 
-          <form onSubmit={this.onInputFormSubmit}>
-            <input
-              className="tempoInput"
-              ref={this.TempoInputRef}
-              value={this.state.tempoInputValue}
-              onChange={this.onTempoInputChange}
-              onFocus={this.onInputFocus}
-              onBlur={this.onInputBlur}
-            />
-          </form>
+            <form onSubmit={this.onInputFormSubmit}>
+              <input
+                className="tempoInput"
+                ref={this.TempoInputRef}
+                value={this.state.tempoInputValue}
+                onChange={this.onTempoInputChange}
+                onFocus={this.onInputFocus}
+                onBlur={this.onInputBlur}
+              />
+            </form>
 
-          <Button
-            className="incrementButton"
-            onClick={this.onIncrementButtonPlusClick}
-          >
-            <i className="fa fa-plus" />
-          </Button>
+            <Button
+              className="incrementButton"
+              onClick={this.onIncrementButtonPlusClick}
+            >
+              <i className="fa fa-plus" />
+            </Button>
 
-          <Button
-            className="playStopButton"
-            onClick={this.onPlayStopButtonClick}
-          >
-            <i className={`fa fa-${this.state.playing ? 'stop' : 'play'}`} />
-          </Button>
+            <Button
+              className="playStopButton"
+              onClick={this.onPlayStopButtonClick}
+            >
+              <i className={`fa fa-${this.state.playing ? 'stop' : 'play'}`} />
+            </Button>
 
-          <TempoSelectGrid setTempo={this.setTempo} />
+            <TempoSelectGrid setTempo={this.setTempo} />
 
-          <div className="volumeControl">
-            <input
-              type="range"
-              step="10"
-              value={this.state.volume}
-              onChange={this.onVolumeInputChange}
-            />
-            <div className="volumeReadout">volume: {this.state.volume}%</div>
+            <div className="volumeControl">
+              <input
+                type="range"
+                step="10"
+                value={this.state.volume}
+                onChange={this.onVolumeInputChange}
+              />
+              <div className="volumeReadout">volume: {this.state.volume}%</div>
+            </div>
+
+            <KeyboardShortcuts />
           </div>
-
-          <KeyboardShortcuts />
-        </div>
+        </Fragment>
       </ThemeProvider>
     );
   }
