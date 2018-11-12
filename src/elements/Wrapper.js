8443235ad61aa2331a87dfafb127bbd25c256341
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-11-12 00:13:18 +0000

    minor changes to Wrapper element styles

--- src/elements/Wrapper.js
+++ src/elements/Wrapper.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  width: 400px;
-  height: 400px;
-  background: ${props => props.theme.main || 'black'};
+  background: ${props => props.theme.bg || 'black'};
 `;
 
 export default Wrapper;
