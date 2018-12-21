e1060b4d8949b77004def556793789e58d10f3a1
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-12-21 18:08:09 +0000

    Sound: fix for chrome autoplay context issue

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -4,7 +4,6 @@ import closedHat from '../audio/LinnHat_C.wav';
 
 class Sound extends Component {
   componentDidMount() {
-    console.log('MOUNTED');
     Tone.Transport.bpm.value = this.props.tempo;
     this.gain = new Tone.Gain(this.props.volume).toMaster();
     this.player = new Tone.Player(closedHat).connect(this.gain);
@@ -15,7 +14,8 @@ class Sound extends Component {
     if (prevProps.tempo !== this.props.tempo)
       Tone.Transport.bpm.value = this.props.tempo;
 
-    if (prevProps.playing !== this.props.playing) Tone.Transport.toggle();
+    if (prevProps.playing !== this.props.playing)
+      Tone.context.ready(Tone.Transport.toggle());
 
     if (prevProps.volume !== this.props.volume)
       this.gain.gain.value = this.props.volume;
