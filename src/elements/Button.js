4339ca04aed541ef7aa8647acc1aa22571d922fa
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-12-24 21:54:46 +0000

    Button element: added default type, added borderWidth prop

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 
-const Button = styled.button`
+const Button = styled.button.attrs({
+  // default - type: 'button'
+  type: props => props.type || 'button'
+})`
   background: transparent;
   outline: none;
   font-size: 1rem;
   padding: 0.5rem;
+  /* default - color: black */
   color: ${props => props.theme.fg || 'black'};
+  /* default - border-color: black */
   border-color: ${props => props.theme.fg || 'black'};
   border-width: ${props =>
     props.noBorder
       ? '0'
-      : props.theme.buttonBorderWidth
-      ? props.theme.buttonBorderWidth
+      : props.borderWidth
+      ? props.borderWidth
+      : props.theme.borderWidth
+      ? props.theme.borderWidth
       : '2px'};
 `;
 
