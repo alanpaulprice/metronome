61ee109ba43e730b9d581424b7bc206053832105
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2018-12-27 16:26:56 +0000

    App: renamed method onAccentButtonClick to onAccentToggleButtonClick

--- src/components/App.js
+++ src/components/App.js
@@ -112,7 +112,7 @@ class App extends Component {
     this.setState({ volume: e.currentTarget.value });
   };
 
-  onAccentButtonClick = () => this.toggleAccent();
+  onAccentToggleButtonClick = () => this.toggleAccent();
 
   // ========== RENDER
 
