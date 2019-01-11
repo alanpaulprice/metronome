7eff1004686f07a24e67049b206dbf17456c1d2e
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-11 22:48:21 +0000

    KeyboardShortcuts: reworked approach to blurring

--- src/components/KeyboardShortcuts.js
+++ src/components/KeyboardShortcuts.js
@@ -1,50 +1,42 @@
+// the or operator (||) is used below to call two functions on one line
+
 import { Component } from 'react';
 
+const blur = () => document.activeElement.blur();
+
 class KeyboardShortcuts extends Component {
-  incrementTempoAndBlur = value => {
-    this.props.incrementTempo(value);
-    document.activeElement.blur();
-  };
-
-  incrementAccentBeatAndBlur = value => {
-    this.props.incrementAccentBeat(value);
-    document.activeElement.blur();
-  };
-
   onKeydown = code =>
     code === 32 // space
-      ? this.props.togglePlaying()
+      ? this.props.togglePlaying() || blur()
       : code === 84 // t
       ? this.props.tempoInputRef.current.focus()
       : code === 37 // arrow left
-      ? this.incrementTempoAndBlur(-1)
+      ? this.props.incrementTempo(-1) || blur()
       : code === 39 // arrow right
+      ? this.props.incrementTempo(1) || blur()
       : code === 77 // m
-      ? this.props.tapTempoButtonRef.current.onTapTempoButtonMouseDown()
+      ? this.props.tapTempoButtonRef.current.onTapTempoButtonMouseDown() ||
+        blur()
       : code === 40 // arrow down
-      ? this.props.incrementVolume(-0.1)
+      ? this.props.incrementVolume(-0.1) || blur()
       : code === 38 // arrow up
-      ? this.props.incrementVolume(0.1)
+      ? this.props.incrementVolume(0.1) || blur()
       : code === 66 // b
       ? this.props.accentBeatInputRef.current.focus()
       : code === 65 // a
-      ? this.props.toggleAccent()
+      ? this.props.toggleAccent() || blur()
       : code === 90 // z
-      ? this.incrementAccentBeatAndBlur(-1)
+      ? this.props.incrementAccentBeat(-1) || blur()
       : code === 88 // x
-      ? this.incrementAccentBeatAndBlur(1)
+      ? this.props.incrementAccentBeat(1) || blur()
       : null;
 
   componentDidMount() {
-    window.addEventListener('keydown', e => {
-      this.onKeydown(e.keyCode);
-    });
+    window.addEventListener('keydown', e => this.onKeydown(e.keyCode));
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', e => {
-      this.onKeydown(e.keyCode);
-    });
+    window.removeEventListener('keydown', e => this.onKeydown(e.keyCode));
   }
 
   render() {
