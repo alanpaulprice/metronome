b68733f1ed1d4e2ff59afff5954d6a38a3cb3c5b
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-13 21:05:51 +0000

    Input: alignment styles

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -6,10 +6,13 @@ const Input = styled.input.attrs(props => ({
   color: ${props => props.color || props.theme.bg || 'black'};
   background: ${props => props.background || props.bg || 'white'};
   font-size: ${props => props.fontSize || '1em'};
+  text-align: ${props => props.textAlign || 'center'};
   border-width: ${props =>
     props.noBorder
       ? '0'
       : props.borderWidth || props.theme.borderWidth || '2px'};
+  justify-self: center;
+  align-self: center;
 `;
 
 export default Input;
