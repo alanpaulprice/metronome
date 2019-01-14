1e5fe08ee5e44670746d9d53d574996548f2ebc4
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-14 23:49:50 +0000

    H1: added optional margin and margin-top

--- src/elements/H1.js
+++ src/elements/H1.js
@@ -4,7 +4,8 @@ const H1 = styled.h1`
   color: ${props => props.theme.fg || 'black'};
   text-align: center;
   font-size: ${props => props.fontSize || '3rem'};
-  margin: ${props => props.margin || '0'};
+  ${props => props.margin && `margin: ${props.margin};`}
+  ${props => props.marginTop && `margin-top: ${props.marginTop};`}
 `;
 
 export default H1;
