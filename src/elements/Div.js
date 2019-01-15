d138cee73daf8608fa2805cb0bb238e75d9fdc98
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-15 17:45:51 +0000

    Div: added optional props for justify-content and align-items

--- src/elements/Div.js
+++ src/elements/Div.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Div = styled.div`
   ${props => props.flex && 'display: flex;'}
   ${props => props.flexDirection && `flex-direction: ${props.flexDirection};`}
+  ${props =>
+    props.justifyContent && `justify-content: ${props.justifyContent};`}
+  ${props => props.alignItems && `align-items: ${props.alignItems};`}
 `;
 
 export default Div;
