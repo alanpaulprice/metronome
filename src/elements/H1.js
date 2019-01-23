4f17abd8e1ec35a527a3d1800c853b574a5df37a
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-23 20:03:56 +0000

    H1: added optional props for fontWeight and letterSpacing

--- src/elements/H1.js
+++ src/elements/H1.js
@@ -4,6 +4,8 @@ const H1 = styled.h1`
   color: ${props => props.theme.fg || 'black'};
   text-align: center;
   font-size: ${props => props.fontSize || '3rem'};
+  ${props => props.fontWeight && `font-weight: ${props.fontWeight};`}
+  ${props => props.letterSpacing && `letter-spacing: ${props.letterSpacing};`}
   ${props => props.margin && `margin: ${props.margin};`}
   ${props => props.marginTop && `margin-top: ${props.marginTop};`}
 `;
