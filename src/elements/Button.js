09f0b86231671d4800eaf011bcac6061b8f53e71
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-24 17:47:22 +0000

    Button: added multiple optional props

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -4,15 +4,21 @@ const Button = styled.button.attrs(props => ({
   type: props.type || 'button'
 }))`
   font-size: ${props => props.fontSize || props.theme.fontSize || '1rem'};
+  text-align: ${props => props.textAlign || 'center'};
   color: ${props => props.color || props.theme.fg || 'black'};
   background: ${props => props.background || props.theme.bg || 'white'};
   padding: ${props => props.padding || '0.5rem'};
-  ${props => props.marginTop && `margin-top: ${props.marginTop};`}
-  text-align: ${props => props.textAlign || 'center'};
   border-color: ${props => props.borderColor || props.theme.fg || 'black'};
   border-width: ${props =>
     props.borderWidth || props.theme.borderWidth || '2px'};
   outline: none;
+  ${props => props.position && `position: ${props.position};`}
+  ${props => props.top && `top: ${props.top};`}
+  ${props => props.right && `right: ${props.right};`}
+  ${props => props.marginTop && `margin-top: ${props.marginTop};`}
+  ${props => props.width && `width: ${props.width};`}
+  ${props => props.height && `height: ${props.height};`}
+  ${props => props.fontWeight && `font-weight: ${props.fontWeight};`}
 `;
 
 export default Button;
