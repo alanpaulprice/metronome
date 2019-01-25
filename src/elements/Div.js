d80c77c2e29f46b1e4c0165a0e8cf45f48560b24
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-25 22:38:39 +0000

    Div: added more optional props

--- src/elements/Div.js
+++ src/elements/Div.js
@@ -8,11 +8,23 @@ const Div = styled.div`
   ${props => props.left && `left: ${props.left};`}
   ${props => props.width && `width: ${props.width};`}
   ${props => props.height && `height: ${props.height};`}
+  ${props => props.minWidth && `min-width: ${props.minWidth};`}
+  ${props => props.minHeight && `min-height: ${props.minHeight};`}
+  ${props => props.margin && `margin: ${props.margin};`}
+  ${props => props.padding && `padding: ${props.padding};`}
+  ${props => props.display && `display: ${props.display};`}
   ${props => props.flexDirection && `flex-direction: ${props.flexDirection};`}
   ${props =>
     props.justifyContent && `justify-content: ${props.justifyContent};`}
   ${props => props.alignItems && `align-items: ${props.alignItems};`}
+  ${props => props.textAlign && `text-align: ${props.textAlign};`}
+  ${props => props.verticalAlign && `vertical-align: ${props.verticalAlign};`}
+  ${props => props.fontSize && `font-size: ${props.fontSize};`}
+  ${props => props.fontWeight && `font-weight: ${props.fontWeight};`}
+  ${props => props.color && `color: ${props.color};`}
   ${props => props.background && `background: ${props.background};`}
+  ${props => props.opacity && `opacity: ${props.opacity};`}
+  ${props => props.transition && `transition: ${props.transition};`}
 `;
 
 export default Div;
