83cd1c4759b4befc86d4623f65410bb34e410b17
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-29 01:01:47 +0000

    Button: added/edited border props, added outline and fontWeight props

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -4,14 +4,16 @@ const Button = styled.button.attrs(props => ({
   type: props.type || 'button'
 }))`
   font-size: ${props => props.fontSize || props.theme.fontSize || '1rem'};
+  ${props => props.fontWeight && `font-weight: ${props.fontWeight};`}
   text-align: ${props => props.textAlign || 'center'};
   color: ${props => props.color || props.theme.fg || 'black'};
   background: ${props => props.background || props.theme.bg || 'white'};
   padding: ${props => props.padding || '0.5rem'};
   border-color: ${props => props.borderColor || props.theme.fg || 'black'};
-  border-width: ${props =>
-    props.borderWidth || props.theme.borderWidth || '2px'};
-  outline: none;
+  ${props => props.border && `border: ${props.border};`};
+  ${props => props.borderWidth && `border-width: ${props.borderWidth};`};
+  ${props => props.borderRadius && `border-radius: ${props.borderRadius};`}
+  outline: ${props => (props.outline ? props.outline : 'none')};
   ${props => props.position && `position: ${props.position};`}
   ${props => props.top && `top: ${props.top};`}
   ${props => props.right && `right: ${props.right};`}
