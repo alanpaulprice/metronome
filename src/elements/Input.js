a6bc63795e98d09d10c7c5692fc3d8c76ef66740
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-01-29 01:00:56 +0000

    Input: added outline, width, margin props, edited border props

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -3,17 +3,18 @@ import styled from 'styled-components';
 const Input = styled.input.attrs(props => ({
   type: props.type || 'text'
 }))`
-  ${props => props.display && `display: ${props.display};`}
-  color: ${props => props.color || props.theme.fg || 'black'};
-  background: ${props => props.background || props.theme.bg || 'white'};
-  font-size: ${props => props.fontSize || '1rem'};
-  text-align: ${props => props.textAlign || 'center'};
-  border-width: ${props =>
-    props.noBorder
-      ? '0'
-      : props.borderWidth || props.theme.borderWidth || '2px'};
-  justify-self: center;
-  align-self: center;
+${props => props.width && `width: ${props.width};`}
+${props => props.margin && `margin: ${props.margin};`}
+${props => props.display && `display: ${props.display};`}
+color: ${props => props.color || props.theme.fg || 'black'};
+background: ${props => props.background || props.theme.bg || 'white'};
+font-size: ${props => props.fontSize || '1rem'};
+text-align: ${props => props.textAlign || 'center'};
+${props => props.border && `border: ${props.border};`};
+${props => props.borderWidth && `border-width: ${props.borderWidth};`};
+outline: ${props => props.outline || 'none'};
+justify-self: center;
+align-self: center;
 `;
 
 export default Input;
