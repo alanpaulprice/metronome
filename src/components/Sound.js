fe384f9b2937ab2c003b84b25240d85345a27b19
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-02-02 22:17:03 +0000

    Sound: moved Loop callback and interval declarations to separate method

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -5,21 +5,26 @@ import closedHat from '../audio/LinnHat_C.wav';
 import openHat from '../audio/LinnHat_O.wav';
 
 class Sound extends Component {
-  componentDidMount() {
-    // set the tempo and prepare audio components
-    Tone.Transport.bpm.value = this.props.tempo;
-    this.gain = new Tone.Gain(this.props.volume).toMaster();
-    this.chh = new Tone.Player(closedHat).connect(this.gain);
-    this.ohh = new Tone.Player(openHat).connect(this.gain);
-
-    this.loop = new Tone.Loop(time => {
+  updateLoop = () => {
+    this.loop.callback = time => {
       this[this.props.accent && !this.props.currentBeat ? 'ohh' : 'chh'].start(
         time,
         0,
         `${this.props.timeSigBeatNoteLength}n`
       );
       this.props.incrementCurrentBeat();
-    }, `${this.props.timeSigBeatNoteLength}n`).start(0);
+    };
+    this.loop.interval = `${this.props.timeSigBeatNoteLength}n`;
+  };
+
+  componentDidMount() {
+    Tone.Transport.bpm.value = this.props.tempo;
+    this.gain = new Tone.Gain(this.props.volume).toMaster();
+    this.chh = new Tone.Player(closedHat).connect(this.gain);
+    this.ohh = new Tone.Player(openHat).connect(this.gain);
+
+    this.loop = new Tone.Loop().start(0);
+    this.updateLoop();
   }
 
   componentDidUpdate(prevProps) {
@@ -32,15 +37,8 @@ class Sound extends Component {
     if (prevProps.volume !== this.props.volume)
       this.gain.gain.value = this.props.volume;
 
-    if (prevProps.timeSigBeatNoteLength !== this.props.timeSigBeatNoteLength) {
-      this.loop.callback = time => {
-        this[
-          this.props.accent && !this.props.currentBeat ? 'ohh' : 'chh'
-        ].start(time, 0, `${this.props.timeSigBeatNoteLength}n`);
-        this.props.incrementCurrentBeat();
-      };
-      this.loop.interval = `${this.props.timeSigBeatNoteLength}n`;
-    }
+    if (prevProps.timeSigBeatNoteLength !== this.props.timeSigBeatNoteLength)
+      this.updateLoop();
   }
 
   render() {
