a4fe3358c797168b25be5395c3e31da13b29ae13
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-02-02 18:44:07 +0000

    Button: removed duplicate rule, reordered rules, added opacity rule

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -3,25 +3,25 @@ import styled from 'styled-components';
 const Button = styled.button.attrs(props => ({
   type: props.type || 'button'
 }))`
+  ${props => props.position && `position: ${props.position};`}
+  ${props => props.top && `top: ${props.top};`}
+  ${props => props.right && `right: ${props.right};`}
+  ${props => props.zIndex && `z-index: ${props.zIndex};`}
+  ${props => props.width && `width: ${props.width};`}
+  ${props => props.height && `height: ${props.height};`}
+  ${props => props.marginTop && `margin-top: ${props.marginTop};`}
+  padding: ${props => props.padding || '0.5rem'};
   font-size: ${props => props.fontSize || props.theme.fontSize || '1rem'};
   ${props => props.fontWeight && `font-weight: ${props.fontWeight};`}
   text-align: ${props => props.textAlign || 'center'};
   color: ${props => props.color || props.theme.fg || 'black'};
   background: ${props => props.background || props.theme.bg || 'white'};
-  padding: ${props => props.padding || '0.5rem'};
   border-color: ${props => props.borderColor || props.theme.fg || 'black'};
   ${props => props.border && `border: ${props.border};`};
   ${props => props.borderWidth && `border-width: ${props.borderWidth};`};
   ${props => props.borderRadius && `border-radius: ${props.borderRadius};`}
   outline: ${props => (props.outline ? props.outline : 'none')};
-  ${props => props.position && `position: ${props.position};`}
-  ${props => props.top && `top: ${props.top};`}
-  ${props => props.right && `right: ${props.right};`}
-  ${props => props.zIndex && `z-index: ${props.zIndex};`}
-  ${props => props.marginTop && `margin-top: ${props.marginTop};`}
-  ${props => props.width && `width: ${props.width};`}
-  ${props => props.height && `height: ${props.height};`}
-  ${props => props.fontWeight && `font-weight: ${props.fontWeight};`}
+  ${props => props.opacity && `opacity: ${props.opacity};`}
   ${props => props.transition && `transition: ${props.transition};`}
 `;
 
