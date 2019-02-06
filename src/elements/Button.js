a61779eab77105602657417f4915c7c5f6984610
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-02-06 20:07:02 +0000

    Button: added bottom, left, and right optional props

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -6,6 +6,9 @@ const Button = styled.button.attrs(props => ({
   ${props => props.position && `position: ${props.position};`}
   ${props => props.top && `top: ${props.top};`}
   ${props => props.right && `right: ${props.right};`}
+  ${props => props.bottom && `bottom: ${props.bottom};`}
+  ${props => props.left && `left: ${props.left};`}
+  ${props => props.right && `right: ${props.right};`}
   ${props => props.zIndex && `z-index: ${props.zIndex};`}
   ${props => props.width && `width: ${props.width};`}
   ${props => props.height && `height: ${props.height};`}
