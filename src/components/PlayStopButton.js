1f285a17a2e40976e6131886d9359616f205fabe
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-02-11 15:57:07 +0000

    PlayStopButton: added container Div to prevent Button taking full width

--- src/components/PlayStopButton.js
+++ src/components/PlayStopButton.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../elements/Button';
 import Icon from '../elements/Icon';
+import Div from '../elements/Div';
 
 class PlayStopButton extends Component {
   onPlayStopButtonClick = () => this.props.togglePlaying();
 
   render() {
     return (
-      <Button
-        onClick={this.onPlayStopButtonClick}
-        borderWidth="0"
-        marginTop="5rem"
-      >
-        <Icon className="material-icons" fontSize="12.5rem">
-          {this.props.playing ? 'stop' : 'play_arrow'}
-        </Icon>
-      </Button>
+      <Div display="flex" justifyContent="center" marginTop="5rem">
+        <Button onClick={this.onPlayStopButtonClick} border="none">
+          <Icon className="material-icons" fontSize="12.5rem">
+            {this.props.playing ? 'stop' : 'play_arrow'}
+          </Icon>
+        </Button>
+      </Div>
     );
   }
 }
